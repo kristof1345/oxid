ba0f86f34c87e3f0876c4287df431c73f7deff97
kristof1345 <kristofnemeth1345@gmail.com>
2025-01-12 22:40:34 +0100

    cursor is alive and wiggling

--- src/editor.rs
+++ src/editor.rs
@@ -1,12 +1,12 @@
-use crossterm::event::{read, Event, KeyCode, KeyEvent, KeyModifiers};
+use crossterm::event::{read, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
 use std::io;
 // use std::panic::{set_hook, take_hook};
 
 mod terminal;
-use terminal::{Position, Terminal};
+use terminal::Terminal;
 
 mod view;
-use view::View;
+use view::{Direction, View};
 
 #[derive(Default)]
 pub struct Editor {
@@ -34,22 +34,33 @@ impl Editor {
     }
 
     pub fn eval_events(&mut self, event: Event) {
-        match event {
-            Event::Key(KeyEvent {
-                code, modifiers, ..
-            }) => match (code, modifiers) {
-                (KeyCode::Char('q'), KeyModifiers::CONTROL) => self.quit = true,
-                // (KeyCode::Char(char), _) => println!("char: {:?}", char),
+        let should_process = match &event {
+            Event::Key(KeyEvent { kind, .. }) => kind == &KeyEventKind::Press,
+            _ => false,
+        };
+
+        if should_process {
+            match event {
+                Event::Key(KeyEvent {
+                    code, modifiers, ..
+                }) => match (code, modifiers) {
+                    (KeyCode::Char('q'), KeyModifiers::CONTROL) => self.quit = true,
+                    (KeyCode::Up, _) => self.view.move_cursor(Direction::Up),
+                    (KeyCode::Down, _) => self.view.move_cursor(Direction::Down),
+                    (KeyCode::Left, _) => self.view.move_cursor(Direction::Left),
+                    (KeyCode::Right, _) => self.view.move_cursor(Direction::Right),
+                    // (KeyCode::Char(char), _) => println!("char: {:?}", char),
+                    _ => {}
+                },
                 _ => {}
-            },
-            _ => {}
+            }
         }
     }
 
     fn refresh_screen(&self) {
         let _ = Terminal::hide_cursor();
         self.view.render();
-        let _ = Terminal::move_cursor_to_pos(Position { col: 0, row: 0 });
+        let _ = Terminal::move_cursor_to_pos(self.view.get_cursor_position());
         let _ = Terminal::show_cursor();
         let _ = Terminal::flush();
     }
--- src/editor/view.rs
+++ src/editor/view.rs
@@ -1,10 +1,24 @@
-use super::terminal::{Size, Terminal};
+use super::terminal::{Position, Size, Terminal};
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[derive(Default, Copy, Clone)]
+pub struct Location {
+    pub x: usize,
+    pub y: usize,
+}
+
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
 
 // #[derive(Default)]
 pub struct View {
     size: Size,
+    location: Location,
 }
 
 impl View {
@@ -23,6 +37,13 @@ impl View {
         }
     }
 
+    pub fn get_cursor_position(&self) -> Position {
+        Position {
+            col: self.location.x,
+            row: self.location.y,
+        }
+    }
+
     fn build_welcome_message(width: usize) -> String {
         let n_a_v = format!("Oxid -- version {}", VERSION);
         let padding = (width - n_a_v.len()) / 2;
@@ -36,12 +57,43 @@ impl View {
         let result = Terminal::print_row(at, line);
         debug_assert!(result.is_ok(), "Failed to render line");
     }
+
+    pub fn move_cursor(&mut self, direction: Direction) {
+        let Location { mut x, mut y } = self.location;
+        let Size { height, width } = self.size;
+
+        match direction {
+            Direction::Up => {
+                if y != 0 {
+                    y = y.saturating_sub(1);
+                }
+            }
+            Direction::Down => {
+                if y < height - 1 {
+                    y = y.saturating_add(1);
+                }
+            }
+            Direction::Right => {
+                if x < width - 1 {
+                    x = x.saturating_add(1);
+                }
+            }
+            Direction::Left => {
+                if x != 0 {
+                    x = x.saturating_sub(1);
+                }
+            }
+        }
+
+        self.location = Location { x, y };
+    }
 }
 
 impl Default for View {
     fn default() -> Self {
         Self {
             size: Terminal::size().unwrap_or_default(),
+            location: Location::default(),
         }
     }
 }
